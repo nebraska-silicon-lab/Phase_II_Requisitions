76084aad38606281151e2dc1dd5ab6fd475a7c4a
Caleb Fangmeier <caleb@fangmeier.tech>
2019-09-10 17:57:02 -0500

    Adds YAML to XLSX converter and a lab supplies requisition

---
+++ yml2xls.py
@@ -0,0 +1,132 @@
+#!/usr/bin/env python3
+"""
+  yml2xls.py
+
+  Uses information in a YAML source file to fill out a UNL Physics Dept.
+  Requisition form.
+
+  Requires: openpyxl, PyYAML
+"""
+import io
+from urllib.request import urlopen
+import datetime
+from os.path import splitext
+
+import openpyxl
+from openpyxl.writer.excel import save_virtual_workbook
+import yaml
+
+
+def chunks(d, n):
+    lst = [(k, v) for (k, v) in d.items()]
+    cs = []
+    for i in range(0, len(lst), n):
+        cs.append({k: v for (k, v) in lst[i : i + n]})
+    return cs
+
+
+def sanitize_filename(filename, ext=None):
+    import string
+
+    allowed_chars = set(string.digits + string.ascii_letters + ".- ")
+    sane_fname = [c for c in filename if c in allowed_chars]
+    sane_fname = "".join(sane_fname).strip()
+    sane_fname = sane_fname.replace(" ", "_")
+    if ext is not None:
+        return "{}.{}".format(sane_fname, ext)
+    else:
+        return sane_fname
+
+
+class UNLRequisition:
+    _blank_form_raw = None
+
+    def __init__(self, src_filename):
+        self.name = splitext(src_filename)[0]
+
+        with open(src_filename, "r") as f:
+            self.src = yaml.load(f)
+
+    def fetch_empty_form(self):
+        """
+        Fetches an blank requisition form from the physics website
+        """
+        global _blank_form_raw
+        blank_req_url = "http://www.unl.edu/physics/docs/Requisition2014.xlsx"
+        if self._blank_form_raw is None:
+            self._blank_form_raw = urlopen(blank_req_url).read()
+        bio = io.BytesIO(self._blank_form_raw)
+        self.workbook = openpyxl.load_workbook(bio)
+
+    def populate_misc_fields(self):
+        ws = self.workbook.active
+        src = self.src
+
+        ws["B11"] = src["vendor"].get("name", "")
+        ws["B13"] = src["vendor"].get("address", "")
+        city = src["vendor"].get("city", "")
+        state = src["vendor"].get("state", "")
+        zip = src["vendor"].get("zip", "")
+        ws["B16"] = f"{city}, {state} {zip}"
+        contact_name = src["vendor"].get("contact_name", "")
+        contact_phone = src["vendor"].get("contact_phone", "")
+        ws["B24"] = f"{contact_name}, {contact_phone}"
+        ws["B22"] = src["vendor"].get("phone", "")
+        ws["F22"] = src["vendor"].get("fax", "")
+        ws["B26"] = src["vendor"].get("url", "")
+
+        ws["E28"] = src.get("delivery_date", "")
+        ws["D43"] = src.get("cost_object", "")
+        ws["B45"] = src.get(
+            "submission_date", datetime.date.today().strftime("%b. %d, %Y")
+        )
+        ws["C47"] = src["requestor_name"]
+        ws["K47"] = src["requestor_phone"]
+        ws["C49"] = src["supervisor_name"]
+
+        ws["B18"] = src["use_for_project"]
+
+    def populate_parts(self, parts):
+        ws = self.workbook.active
+        for i, (part, info) in enumerate(parts.items()):
+            i = str(i + 32)
+            ws["A" + i] = part
+            ws["B" + i] = info.get("desc", "")
+            ws["L" + i] = info.get("quantity", 1)
+            ws["Q" + i] = info.get("unit_price", "N/A")
+
+    def place_sheet_number(self, i, n):
+        ws = self.workbook.active
+        ws["A52"] = "Sheet {} of {}".format(i, n)
+
+    def save_form(self):
+        """
+        Creates a zip file containing the one or more xlsx formatted
+        requisitions as required by UNL Physics purchasing. Creates the file
+        in memory and then returns it as bytes.
+        """
+        parts_chunked = chunks(self.src["items"], 10)
+        for i, parts_chunk in enumerate(parts_chunked):
+            self.fetch_empty_form()
+            self.populate_parts(parts_chunk)
+            self.populate_misc_fields()
+            self.place_sheet_number(i + 1, len(parts_chunked))
+
+            if len(parts_chunked) > 1:
+                fname = f"{self.name}_{i:02d}.xlsx"
+            else:
+                fname = f"{self.name}.xlsx"
+            with open(fname, "wb") as f:
+                f.write(save_virtual_workbook(self.workbook))
+
+
+if __name__ == "__main__":
+    from argparse import ArgumentParser
+
+    parser = ArgumentParser("yml2xls")
+    parser.add_argument("input_file")
+
+    args = parser.parse_args()
+
+    form = UNLRequisition(args.input_file)
+    form.save_form()
